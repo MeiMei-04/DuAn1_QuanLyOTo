ec52fecc4d1e0b7ce18213783f6b7ee8157e0b4f
Hieu <Hieu@EmHieuDepTrai>
2023-11-23 22:12:18 +0700

    123123

--- src/Hepler/DateHelper.java
+++ src/Hepler/DateHelper.java
@@ -56,36 +56,6 @@ public class DateHelper {
         now.setTime(now.getTime() + days * 24 * 60 * 60 * 1000);
         return now;
     }
-
-    // hàm kiểm tra xem có phải ngày tháng k
-    public static boolean isValidDate(String dateString) {
-        SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
-        try {
-            df.parse(dateString);
-            return true;
-        } catch (ParseException e) {
-            return false;
-        }
-    }
-
-    // hàm kiểm tra định dạng alpha
-    public static boolean isAlphabetic(String input) {
-        return !input.isEmpty() && input.matches("[\\p{L} ]+");
-    }
-
-    //hàm kiểm tra năm
-    public static boolean isMoreThan16YearsAgo(LocalDate date) {
-        LocalDate now = LocalDate.now();
-        Period period = Period.between(date, now);
-        return period.getYears() > 16;
-    }
-
-    public static boolean isMoreThan5DaysFromNow(LocalDate date) {
-        LocalDate now = LocalDate.now();
-        long daysBetween = ChronoUnit.DAYS.between(now, date);
-        return daysBetween >= 5;
-    }
-
     //convert to localdate
     public static LocalDate toLocalDate(String date, String pattern) {
         try {
@@ -95,14 +65,5 @@ public class DateHelper {
             throw new RuntimeException(e);
         }
     }
-    //kiểm tra chuỗi có phải 1 số hay không
-
-    public static boolean isNumeric(String str) {
-        try {
-            Double.parseDouble(str);
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
-        }
-    }
+    
 }
